c0ab88da42d15b1a78d707a3305ef918007566ef
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2026-02-11 11:06:14 +0530

    Allow mixed content for apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'smartsme-fe',
-  webDir: 'www'
+    appId: 'io.ionic.starter',
+    appName: 'smartsme-fe',
+    webDir: 'www',
+    android: {
+    	 allowMixedContent: true 
+    }
 };
 
 export default config;
