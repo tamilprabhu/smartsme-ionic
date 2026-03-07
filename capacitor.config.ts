17669a02bdc5d33607a5c8a48aaaab59f599ef71
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2026-03-07 10:45:26 +0530

    feat(android): regenerate Capacitor icons/splash assets and rename app to SmartSME

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'io.ionic.starter',
-    appName: 'smartsme-fe',
+    appName: 'SmartSME',
     webDir: 'www',
     android: {
     	 allowMixedContent: true 
